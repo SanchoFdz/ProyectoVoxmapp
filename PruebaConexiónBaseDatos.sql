9ca90ae8820d6b46c188094520c0675120c57502
ZihiFredo <70400929+ZihiFredo@users.noreply.github.com>
2021-04-28 21:19:21 -0500

    Add files via upload

---
+++ PruebaConexiónBaseDatos.sql
@@ -0,0 +1,39 @@
+CREATE TABLE encuesta (
+   pregunta1 varchar(50),
+   pregunta2 varchar(50),
+   pregunta3 varchar(50),
+   pregunta4 varchar(50),
+   pregunta5 varchar(50),
+   pregunta6 varchar(50),
+   pregunta7 varchar(50),
+   pregunta8 varchar(50),
+   pregunta9 varchar(50),
+   pregunta10 varchar(50),
+   pregunta11 varchar(50),
+   pregunta12 varchar(50),
+   pregunta13 varchar(50),
+   pregunta14 varchar(50),
+   pregunta15 varchar(50),
+   pregunta16 varchar(50),
+   pregunta17 varchar(50),
+   pregunta18 varchar(50),
+   pregunta19 varchar(50),
+   pregunta20 varchar(50),
+   pregunta21 varchar(50),
+   pregunta22 varchar(50),
+   pregunta23 varchar(50),
+   pregunta24 varchar(50),
+   pregunta25 varchar(50),
+   pregunta26 varchar(50),
+   pregunta27 varchar(50),
+   pregunta28 varchar(50),
+   pregunta29 varchar(50),
+   pregunta30 varchar(50),
+   pregunta31 varchar(50),
+   pregunta32 varchar(50),
+   pregunta33 varchar(50)
+);
+
+
+
+select * from encuesta
